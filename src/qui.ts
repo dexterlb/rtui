a3651f0e44e5a1573faf1d6dbe836cb8d80742b8
dexterlb <dexterlb@qtrp.org>
2026-08-01 20:50:53 +0300

    wip

--- src/qui.ts
+++ src/qui.ts
@@ -8,15 +8,26 @@ type EventHandler = (evt: Event) => Promise<void>;
 interface Action {
     t: "action"
     handler: ActionHandler
+    reset?: () => void
+    reset_after?: number
+    bucket?: string
 }
+
 interface Stop {
     t: "stop"
 }
 
 class QUI {
     private queue: AsyncQueue<Action | Stop> = new AsyncQueue<Action | Stop>;
-    public add(handler: ActionHandler, bucket?: string) {
+    public add(opts: { handler: ActionHandler, bucket?: string, reset?: () => void, reset_after?: number }) {
+        const { handler, bucket, reset, reset_after } = opts;
+
         if (bucket !== undefined) {
+            const prev_resetter = this.resetters.get(bucket)
+            if (prev_resetter) {
+                clearTimeout(prev_resetter)
+            }
+
             const bucket_handler = async () => {
                 this.buckets.delete(bucket);
                 return await handler();
@@ -27,7 +38,7 @@ class QUI {
                 // override the previous handler
                 bucket_action.handler = bucket_handler;
             } else {
-                bucket_action = { t: "action", handler: bucket_handler }
+                bucket_action = { t: "action", bucket: bucket, handler: bucket_handler, reset: reset, reset_after: reset_after }
                 this.buckets.set(bucket, bucket_action);
                 this.queue.push(bucket_action);
             }
@@ -35,7 +46,7 @@ class QUI {
             return
         }
 
-        this.queue.push({ t: "action", handler: handler});
+        this.queue.push({ t: "action", bucket: bucket, handler: handler, reset: reset, reset_after: reset_after});
     }
 
     public stop() {
@@ -43,7 +54,10 @@ class QUI {
     }
 
     public addEventListener(el: HTMLElement, type: string, handler: EventHandler) {
-        el.addEventListener(type, (evt) => this.add(async () => { await handler(evt); }, el.id))
+        el.addEventListener(type, (evt) => this.add({
+            handler: async () => { await handler(evt); },
+            bucket: el.id,
+        }))
     }
 
     public async loop(err_handler: (err: any) => Promise<void>) {
@@ -52,7 +66,16 @@ class QUI {
             if (baba.t == 'action') {
                 try {
                     await baba.handler();
+                    if (baba.reset && baba.reset_after) {
+                        const resetter = setTimeout(baba.reset, baba.reset_after);
+                        if (baba.bucket !== undefined) {
+                            this.resetters.set(baba.bucket, resetter);
+                        }
+                    }
                 } catch (err) {
+                    if (baba.reset) {
+                        baba.reset();
+                    }
                     await err_handler(err);
                 }
             }
@@ -63,4 +86,5 @@ class QUI {
     }
 
     private buckets: Map<string, Action> = new Map();
+    private resetters: Map<string, ReturnType<typeof setTimeout>> = new Map();
 }
